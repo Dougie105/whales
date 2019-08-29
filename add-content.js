86893d4a76e5872d2328aebe91e4ef64eeb95de3
Terrell Douglas <terrelloydouglas@yahoo.com>
2019-08-29 14:09:41 -0700

    project

--- add-content.js
+++ add-content.js
@@ -1,13 +1,14 @@
 function myFunction(){
 
     var name = prompt ('What\'s your name?');
+
     var answer = confirm  ('Do you want to save whales?');
     var output = 'Hello ' + name + '!';
     var output2 = 'Wow ' + name + '...';
 
-    if (answer) {
+    } else(answer) {
         output = output +  ' Thank you for your support!'
-    } else { 
+    } if else { 
         output = output2 + ' A whale just died because of you...'
     }
     return output;
